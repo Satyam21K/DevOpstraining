69635fe19415938b833723141a0c0868c923bae0
Satyam21K <67287995+Satyam21K@users.noreply.github.com>
2022-10-04 16:14:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,12 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    int a=4,b=6,i;
+    int a=4,b,i;
     
     for(i=0;i<a;i++){
+      for(b=0;b<i;b++){
     System.out.println("*\n");
-    }
+    }}
       
     System.out.println("My name is satyam");
   }
