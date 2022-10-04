06809df9e353da2f0799c8cf946477d8e3ed6f2e
Satyam21K <67287995+Satyam21K@users.noreply.github.com>
2022-10-04 16:36:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,16 +3,7 @@
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
 public class HelloWorld {
-  public static void main(String[] args) {
-    int a=5,b,i;
-    
-    for(int i=0; i<a; i++)   
-{   
-for(int j=0; j<=i; j++)   
-{   
-System.out.print("* ");   
-}   
-System.out.println();   
+  public static void main(String[] args) {  
     System.out.println("My name is satyam");
   }
 }
